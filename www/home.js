4afdc91a930af47e7cb8731f896a8116b59bacbd
kareem-g <kareemgameel2051@gmail.com>
2023-08-04 17:22:25 +0300

    Fixing dynamic room id

--- www/home.js
+++ www/home.js
@@ -1,16 +1,14 @@
 /* globals  Vue */
 Vue.createApp({
-  data() {
-    return {
-      roomId: "",
-    };
-  },
-  methods: {
-    generateRandomRoomId() {
-      this.roomId = Math.random().toString(36).substr(2, 6);
-    },
-    goToRoom() {
-      window.location = "/" + this.roomId;
-    },
-  },
-}).mount("#app");
+	data() {
+	  return {
+		roomId: "",
+	  };
+	},
+	methods: {
+	  generateRandomRoomId() {
+		this.roomId = Math.random().toString(36).substr(2, 6);
+	  },
+	},
+  }).mount("#app");
+  
